ac1f0ad18020151fca4c3904689fe47c6f157d02
Deepu316 <110838306+Deepu316@users.noreply.github.com>
2022-08-10 12:31:02 +0530

    Update telethon_events.py

--- telethon_events.py
+++ telethon_events.py
@@ -4,8 +4,8 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"Spider-Man/Telethon/{plugin_name}.py")
-    name = "Spider-Man.Telethon.{}".format(plugin_name)
+    path = Path(f"Flash/Telethon/{plugin_name}.py")
+    name = "Flash.Telethon.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
