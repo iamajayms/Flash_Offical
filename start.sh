8e5b03de491ad165cca9d48b38b4fcc01b29dd82
Deepu316 <110838306+Deepu316@users.noreply.github.com>
2022-08-10 12:30:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/BXBotz2021/Spider-Official.git /Spider-Official
+  git clone https://github.com/Deepu316/Flash_Offical.git /Flash_offical
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Spider-Official
+  git clone $UPSTREAM_REPO /Flash_offical
 fi
 cd /Spider-Official
 pip3 install -U -r requirements.txt
